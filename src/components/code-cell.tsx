9ffc60ba98b5eca25772d646379b7a5740dbbb64
Cash Myers <cmyers880@gmail.com>
2022-11-01 17:10:54 -0500

    feat: add loading "spinner" for bundling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import './code-cell.css';
 
 interface CodeCellProps {
   cell: Cell;
@@ -15,12 +16,18 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1250)
     return () => {
       clearTimeout(timer);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
@@ -32,7 +39,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {
+          !bundle || bundle.loading 
+          ? (<div className='progress-cover'>
+            <progress className='progress is-smal is-primary' max="100">
+              Loading
+            </progress>
+          </div>)
+          : (<Preview code={bundle.code} err={bundle.err} />)
+        }
       </div>
     </Resizable>
   );
