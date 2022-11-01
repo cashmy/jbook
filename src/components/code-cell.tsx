5e9bd18524801775966ab3d909e4289ea65391d4
Cash Myers <cmyers880@gmail.com>
2022-11-01 18:20:16 -0500

    feat: Add show() to dsply vars, text, js, & cmpnts

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,9 +17,39 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
+
+    // Add "show" function to the top of the bundle
+    // _React and _ReactDOM are used to avoid naming conflicts
+    // Both variables (React and _React) point to the same package.
+    //    ESBuild is been configured to use these particular names
+    //    instead createReactElement and createReactDOM
+    const showFunc = `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+        var show = (value) => {
+          const root = document.querySelector('#root');
+          if (typeof value === 'object') {
+            if (value.$$typeof && value.props) { 
+              _ReactDOM.render(value, root);
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
+          } else {
+            root.innerHTML = value;
+          }
+        };
+      `;
+    const showFuncNoop = 'var show = () => {}';
+
     const cumulativeCode = [];
+
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
@@ -28,7 +58,6 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     }
     return cumulativeCode;
   });
-  // console.log(cumulativeCode);
 
   useEffect(() => {
     if (!bundle) {
