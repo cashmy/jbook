b8552ee0e8b7596f755aa999bd5b83a97519c1db
Cash Myers <cmyers880@gmail.com>
2022-11-02 11:24:48 -0500

    fix: final clean up - ready for publishing

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -108,9 +108,8 @@ export const fetchCells = () => {
 
 export const saveCells = () => {
     return async (dispatch: Dispatch<Action>, getState: () => RootState) => {
-        const { cells: { data, order } } = getState();
-        const cells = order.map((id) => data[id]);
-
+      const { cells: { data, order }, } = getState();
+      const cells = order.map((id) => data[id]);
         try {
             await axios.post('/cells', { cells });
         } catch (err) {
